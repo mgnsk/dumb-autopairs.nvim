13b643b6d5d5c774144d0dded43b2ace17530be3
Magnus Kokk <magnus@kokk.eu>
2024-10-28 12:56:22 +0200

    Split up code into functions

--- lua/dumb-autopairs/init.lua
+++ lua/dumb-autopairs/init.lua
@@ -82,8 +82,108 @@ local function has_comma_suffix(s)
 	return hassuffix(s, { "," })
 end
 
+--- @param pair Pair
+local function on_open_quote(pair)
+	local left, right = get_surrounding()
+
+	if right:sub(1, 1) == pair.right then
+		-- Handle manually closing the quote.
+		feedkeys("<Right>")
+	elseif left == "" and right == "" then
+		feedkeys(pair.left .. pair.right .. "<Left>")
+	elseif (has_operator_suffix(left) or has_comma_suffix(left)) and right == "" then
+		feedkeys(pair.left .. pair.right .. "<Left>")
+	elseif has_open_brace_suffix(left) or (has_comma_suffix(left) and has_close_brace_prefix(right)) then
+		feedkeys(pair.left .. pair.right .. "<Left>")
+	else
+		feedkeys(pair.left)
+	end
+end
+
+--- @param pair Pair
+local function on_open_brace(pair)
+	local left, right = get_surrounding()
+
+	if right == "" then
+		feedkeys(pair.left .. pair.right .. "<Left>")
+	elseif has_open_brace_suffix(left) or has_close_brace_prefix(right) or has_open_brace_prefix(right) then
+		feedkeys(pair.left .. pair.right .. "<Left>")
+	else
+		feedkeys(pair.left)
+	end
+end
+
+--- @param pair Pair
+local function on_close_brace(pair)
+	local _, right = get_surrounding()
+
+	if right:sub(1, 1) == pair.right then
+		feedkeys("<Right>")
+	else
+		feedkeys(pair.right)
+	end
+end
+
+--- @param config Config
+local function on_enter(config)
+	local left, right = get_surrounding()
+
+	local found = false
+
+	for _, pair in ipairs(config["pairs"]) do
+		if hassuffix(left, { pair.left }) and hasprefix(right, { pair.right }) then
+			found = true
+			break
+		end
+	end
+
+	if found then
+		-- Note: assumes these settings:
+		-- vim.o.autoindent = true
+		-- vim.cmd("filetype plugin indent on")
+		feedkeys("<CR><Up><End><CR>")
+	else
+		feedkeys("<CR>")
+	end
+end
+
+--- @param config Config
+local function on_backspace(config)
+	local left, right = get_surrounding()
+
+	local found = false
+	local del_count = 1
+
+	for _, pair in ipairs(config["pairs"]) do
+		if left:sub(-1) == pair.left and hasprefix(right, { pair.right }) then
+			-- Find how many <Del> we need to reach pair.right.
+			local idx, _ = right:find(pair.right)
+			if idx ~= nil then
+				del_count = idx
+			end
+
+			found = true
+			break
+		end
+	end
+
+	if found then
+		feedkeys(string.rep("<Del>", del_count) .. "<BS>")
+	else
+		feedkeys("<BS>")
+	end
+end
+
 local M = {}
 
+--- @class (exact) Pair
+--- @field left string
+--- @field right string
+
+--- @class (exact) Config
+--- @field pairs Pair[]
+
+--- @type Config
 local default_config = {
 	pairs = {
 		{
@@ -113,6 +213,7 @@ local default_config = {
 	},
 }
 
+--- @param config Config
 function M.setup(config)
 	config = vim.tbl_extend("force", default_config, config or {})
 
@@ -122,95 +223,25 @@ function M.setup(config)
 		end
 
 		if pair.left == pair.right then
-			-- Handle completing quotes.
 			vim.keymap.set("i", pair.left, function()
-				local left, right = get_surrounding()
-
-				if right:sub(1, 1) == pair.right then
-					-- Handle manually closing the quote.
-					feedkeys("<Right>")
-				elseif left == "" and right == "" then
-					feedkeys(pair.left .. pair.right .. "<Left>")
-				elseif (has_operator_suffix(left) or has_comma_suffix(left)) and right == "" then
-					feedkeys(pair.left .. pair.right .. "<Left>")
-				elseif has_open_brace_suffix(left) or (has_comma_suffix(left) and has_close_brace_prefix(right)) then
-					feedkeys(pair.left .. pair.right .. "<Left>")
-				else
-					feedkeys(pair.left)
-				end
-			end)
+				on_open_quote(pair)
+			end, { desc = "Complete quotes" })
 		else
-			-- Handle completing braces. It does not attempt to "wrap" anything in braces.
 			vim.keymap.set("i", pair.left, function()
-				local left, right = get_surrounding()
-
-				if right == "" then
-					feedkeys(pair.left .. pair.right .. "<Left>")
-				elseif has_open_brace_suffix(left) or has_close_brace_prefix(right) or has_open_brace_prefix(right) then
-					feedkeys(pair.left .. pair.right .. "<Left>")
-				else
-					feedkeys(pair.left)
-				end
-			end)
-
-			-- Handle manually closing the brace.
+				on_open_brace(pair)
+			end, { desc = "Complete braces" })
 			vim.keymap.set("i", pair.right, function()
-				local _, right = get_surrounding()
-
-				if right:sub(1, 1) == pair.right then
-					feedkeys("<Right>")
-				else
-					feedkeys(pair.right)
-				end
-			end)
+				on_close_brace(pair)
+			end, { desc = "Handle manual brace close" })
 		end
 	end
 
-	-- Handle enter key between braces.
 	vim.keymap.set("i", "<CR>", function()
-		local left, right = get_surrounding()
-
-		local found = false
-
-		for _, pair in ipairs(config["pairs"]) do
-			if hassuffix(left, { pair.left }) and hasprefix(right, { pair.right }) then
-				found = true
-				break
-			end
-		end
-
-		if found then
-			feedkeys("<CR><Up><End><CR>")
-		else
-			feedkeys("<CR>")
-		end
+		on_enter(config)
 	end, { desc = "Indent when pressing enter between braces" })
 
-	-- Handle backspace key between braces.
 	vim.keymap.set("i", "<BS>", function()
-		local left, right = get_surrounding()
-
-		local found = false
-		local del_count = 1
-
-		for _, pair in ipairs(config["pairs"]) do
-			if left:sub(-1) == pair.left and hasprefix(right, { pair.right }) then
-				-- Find how many <Del> we need to reach pair.right.
-				local idx, _ = right:find(pair.right)
-				if idx ~= nil then
-					del_count = idx
-				end
-
-				found = true
-				break
-			end
-		end
-
-		if found then
-			feedkeys(string.rep("<Del>", del_count) .. "<BS>")
-		else
-			feedkeys("<BS>")
-		end
+		on_backspace(config)
 	end, { desc = "Delete both braces when pressing backspace between braces" })
 end
 
